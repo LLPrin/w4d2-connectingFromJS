9ddc587d44f0e641899bff7adf0031502b248715
LLPrin <LLPrin@users.noreply.github.com>
2016-11-03 22:32:40 +0000

    committing what I have

---
+++ famous_ppl.sql
@@ -0,0 +1,6 @@
+CREATE TABLE people (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  born DATE NOT NULL
+);
+
---
+++ seed.sql
@@ -0,0 +1,6 @@
+
+--PEOPLE
+INSERT INTO "people" (id, name, born) VALUES(1, 'Amal Alamuddin', to_date('1978-02-03', 'YYYY-MM-DD'));
+INSERT INTO "people" (id, name, born) VALUES(1, 'Angus Young', to_date('1955-03-31', 'YYYY-MM-DD'));
+INSERT INTO "people" (id, name, born) VALUES(1, 'Aretha Franklin', to_date('1942-03-25', 'YYYY-MM-DD'));
+
